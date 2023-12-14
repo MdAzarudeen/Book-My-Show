e113c093a818d7340bda639d972cc93f70129240
MdAzarudeen <mohazar671@gmail.com>
2023-12-14 23:44:21 +0530

    first commit

---
+++ src/main/java/BookmyShowProject/Repository/ShowSeatRepository.java
@@ -0,0 +1,7 @@
+package BookmyShowProject.Repository;
+
+import BookmyShowProject.Models.ShowSeat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShowSeatRepository extends JpaRepository<ShowSeat,Integer> {
+}
---
+++ src/test/java/BookmyShowProject/example/BookMyShow/demo/BookmyShowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package BookmyShowProject.example.BookMyShow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyShowProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
